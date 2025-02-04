884365091ed4c01f9fa3135795769fb0fd53b662
zelmimouni <zakarya.elmimouni@ensae.fr>
2025-02-04 10:37:10 +0000

    Corrige l'erreur qui empêchait l'exécution

--- titanic.py
+++ titanic.py
@@ -8,8 +8,8 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.ensemble import RandomForestClassifier
 import time
 import os
+import seaborn as sns
 
-os.chdir('/home/coder/work/ensae-reproductibilite-application')
 TrainingData = pd.read_csv('data.csv')
 
 TrainingData.head()
